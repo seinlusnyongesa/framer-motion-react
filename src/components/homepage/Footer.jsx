9f5a0c7f3b40a1a05b198aa8fe4071fec7988dbb
seinlusnyongesa <seinlus044@gmail.com>
2022-07-29 16:22:58 +0300

    added a footer

--- src/components/homepage/Footer.jsx
+++ src/components/homepage/Footer.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 import style from "./footer.module.css";
+import {
+  FaFacebook,
+  FaGithub,
+  FaHeart,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className={style.container}>
       <div className={style.content}>
-        <h3 className={style.title}>Skills</h3>
-        <ul className={style.tools}>
-          <li>
-            <h5>reactjs</h5>
+        <ul className={style.socials}>
+          <li className={style.icon}>
+            <a href="#">
+              <FaFacebook />
+            </a>
           </li>
-          <li>
-            <h5>nodejs</h5>
+          <li className={style.icon}>
+            <a href="#">
+              <FaTwitter />
+            </a>
           </li>
-          <li>
-            <h5>expressjs</h5>
+          <li className={style.icon}>
+            <a href="#">
+              <FaInstagram />
+            </a>
           </li>
-          <li>
-            <h5>mongodb</h5>
-          </li>
-          <li>
-            <h5>mysql</h5>
+          <li className={style.icon}>
+            <a href="#">
+              <FaGithub />
+            </a>
           </li>
         </ul>
+        <p>
+          &copy;2022 made with{" "}
+          <span>
+            <FaHeart />
+          </span>{" "}
+          by seinlus nyongesa
+        </p>
       </div>
     </div>
   );
