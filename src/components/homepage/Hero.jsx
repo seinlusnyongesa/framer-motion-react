9d1e1ce043adcc7baf62b1872e73ae3ea2545add
seinlusnyongesa <seinlus044@gmail.com>
2022-07-29 21:43:14 +0300

    some animation on hero section

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import style from "./hero.module.css";
 import { FaChevronDown } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const contentVariant = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1,
+      duration: 4,
+      when: "beforeChildren",
+    },
+  },
+};
+
+const buttonVariant = {
+  visible: {
+    y: 10,
+    transition: {
+      when: "afterParent",
+      duration: 1,
+      yoyo: Infinity,
+    },
+  },
+};
+
 const Hero = () => {
   return (
     <div className={style.container}>
-      <div className={style.content}>
+      <motion.div
+        variants={contentVariant}
+        initial="hidden"
+        animate="visible"
+        className={style.content}
+      >
         <h4>Hello, I'm seinlus nyongesa.</h4>
         <h1>a software engineer.</h1>
         <p>
@@ -12,10 +42,10 @@ const Hero = () => {
           you can also say hi.
         </p>
 
-        <button>
+        <motion.button variants={buttonVariant}>
           <FaChevronDown />{" "}
-        </button>
-      </div>
+        </motion.button>
+      </motion.div>
     </div>
   );
 };
